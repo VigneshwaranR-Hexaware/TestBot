d6411b7cd60fc0bb939f4e046a7e2116d6cf2ba0
ShrutiVi-hexaware <ShrutiVi.hexaware@gmail.com>
2017-07-12 14:15:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var prompt = require('prompt')
 var access_token = "fcc2d12400644c589538c72c41a40955";
 var app = apiai(access_token);
 
-
+var options = {
+    sessionId: '567yh8'
+};
 run_main('hi');
 
 function run_main(query1) {
-var request = app.textRequest(query1);
+var request = app.textRequest(query1,options);
 //console.log(request);
 request.on('response', function(response) {
 
