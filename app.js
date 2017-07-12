3a3207b0877f14ee3a5ca3dfeb56cd5aa1a2d739
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-07-12 11:40:28 +0530

    included index.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express();
 const https= require('https');
 var http = require('http');
 const JSONbig = require('json-bigint');
-const async = require('async');
-const requestPromise = require('request-promise');
+
 const developerAccesstoken='fcc2d12400644c589538c72c41a40955';
 
 var requestOptions={
       host:'https://api.api.ai/v1/query?v=20150910',
+    
       method: 'POST',
       headers: {
         'Authorization':'Bearer fcc2d12400644c589538c72c41a40955 ',
@@ -26,3 +26,6 @@ var requestObj=https.request(requestOptions,function(req,res){
     console.log("Hi I am Triggered");
     console.log(req);
 });
+
+
+curl -H "Content-Type: application/json; charset=utf-8" -H "Authorization: Bearer fcc2d12400644c589538c72c41a40955" --data "{'query':'Hi', 'timezone':'GMT-5', 'lang':'en', 'sessionId':'1234567890'}" "https://api.api.ai/v1/query?v=20150910"
