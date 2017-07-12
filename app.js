4350b4edca78cfcdfd631e5a2b970dd9fc5f86ab
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-07-12 12:24:01 +0530

    included index.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+var apiai = require('apiai');
 const bodyParser = require('body-parser');
-const app = express();
+const developerAccesstoken='fcc2d12400644c589538c72c41a40955';
+const app = apiai(developerAccesstoken);
 const https= require('https');
 var http = require('http');
 const JSONbig = require('json-bigint');
 
-const developerAccesstoken='fcc2d12400644c589538c72c41a40955';
+
 
 var requestOptions={
       host:'https://api.api.ai/v1/query?v=20150910',
@@ -21,9 +23,12 @@ var requestOptions={
      }
 };
 
-var requestObj=https.request(requestOptions,function(req,res){
+var requestObj=https.request(requestOptions,function(response){
+    response.on("data",function(data){
     console.log("Hi I am Triggered");
     console.log(req);
+    });
+    response.on("error",function(error){
+        console.log(error);
+    });
 });
-
-
