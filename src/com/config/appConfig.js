80912c376623e654d138ab6a05c9161afdd5ffac
Jeyam pandi Bose (jp) <32128@hexaware.com>
2017-07-14 11:27:30 +0530

    fs creation

--- src/com/config/appConfig.js
+++ src/com/config/appConfig.js
@@ -1,1 +1,2 @@
 module.exports.developerAccessToken='Bearer fcc2d12400644c589538c72c41a40955';
+module.exports.expectedOutputFile='./data/sampleExpectedResponse.txt'
--- src/com/io/responseChecker.js
+++ src/com/io/responseChecker.js
@@ -1,48 +1,56 @@
 var LineReader = require('linereader');
-var lr = new LineReader('./sampleExpectedResponse.txt');
+var appConfig = require('../config/appConfig.js');
+var lr = new LineReader(appConfig.expectedOutputFile);
 const responseMap = new Map();
 
 function checkForIOError() {
   lr.on('error', function (err) {
-    //console.log(err);
+    console.log(err);
     lr.close();
+    return true;
   });
+  return false;
 }
 
 function main() {
     //checkForIOError
-    checkForIOError();
-    var responseName = null;
-    lr.on('line', function (lineno, line) {
-        if(line.match("StaticResponse")) {
-            responseName = null;
-            var tempStr = line.split("::");
-            if(tempStr && tempStr.length <= 2) {
-                responseName = tempStr[1];
-            }
-        } else if (responseName) {
-              var responseArray = responseMap.get(responseName);
-              if(!responseArray) {
-                  responseArray = [];
-                  responseMap.set(responseName, responseArray);
+    if(!checkForIOError()) {
+      var responseName = null;
+      lr.on('line', function (lineno, line) {
+          if(line.match("StaticResponse")) {
+              responseName = null;
+              var tempStr = line.split("::");
+              if(tempStr && tempStr.length <= 2) {
+                  responseName = tempStr[1];
               }
-              responseArray.push(line);
-        }
-    });
+          } else if (responseName) {
+                var responseArray = responseMap.get(responseName);
+                if(!responseArray) {
+                    responseArray = [];
+                    responseMap.set(responseName, responseArray);
+                }
+                responseArray.push(line);
+          }
+      });
 
-    lr.on('end', function () {
-      console.log("Resposne file has been pushed into memory");
-    });
+      lr.on('end', function () {
+        console.log("Resposne file has been pushed into memory");
+        var tst = checkResponse("WelcomeIntent", "Welcome, How could I assist you?");
+        console.log(tst);
+      });
+    } else {
+      console.log("IO Error occured while reading "+appConfig.expectedOutputFile);
+    }
+
 
 }
 
 function checkResponse(tcName, responseLine) {
     var responseArray = responseMap.get(tcName);
-    if(!responseArray) {
-       //return responseArray.indexOf(responseLine);
+    if(responseArray) {
+        return (responseArray.indexOf(responseLine) > -1);
     }
-    return "false";
+    return false;
 }
 
 main();
-checkResponse("WelcomeIntent", "Welcome, How could I assist you?");
