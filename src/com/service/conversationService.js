0d064245b577f2e6cd1a5dc0cab2d56e5ac4cb25
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-07-17 20:29:11 +0530

    Update conversationService.js

--- src/com/service/conversationService.js
+++ src/com/service/conversationService.js
@@ -1,70 +1,69 @@
 //var inputfile = require('../data/VFSComplaintRequest.txt');
 var LineReader = require('linereader');
-
-var jsUtil=require('util');
-
 var appConfig = require('../config/appConfig.js');
 var logService=require('./logService');
 var queryService=require('./queryService');
-var util=require('../config/util.js');
-
-const responseMap = new Map();
-
-
+//var lr = new LineReader(appConfig.expectedOutputFile);
+console.log('to call function')
 processRequest();
 
-
-
-function processRequest() {
-const fs = require('fs');
+function processRequest(){
+//  const readline = require('readline');
+  const fs = require('fs');
 var currentLine=null;
 var expectedResponse=[];
 var failedLines=[];
 var responseFromApi=null;
 var tcPassCount=0;
 var tcFailCount=0;
-
-var rl = new LineReader(appConfig.inputfile);
-var custLineNo = -1;
-  rl.on('line',function(lineno,line) {
-    currentLine=line;
-
-        var prefix=currentLine.split(":");
-
-        if(prefix[0]=='Cust'){
-                //var queryServ = new queryProcessing(prefix[1]);
-                custLineNo = lineno;
-                console.log("LINE NO"+custLineNo);
-                queryService.queryProcessing(prefix[1], lineno, responseMap);
-                expectedResponse=new Array();
-          }else if (prefix[0]=='Bot') {
-              console.log("LINE NO in bot"+custLineNo);
-                pushToMap(custLineNo, prefix[1]);
-          }
-/*if(expectedResponse.length>0){
-var result=checkResponse(responseFromApi,expectedResponse);
-  if(result){
-    tcPassCount++;
-  }
-  else{
-    failedLines.push(lineno);
-    tcFailCount++;
-  }
-}*/
+  console.log(appConfig.inputfile);
+fs.readFileSync(appConfig.inputfile).toString().split('\n').forEach(function (lineno,line) { 
+    console.log(line);
+    console.log(lineno);
+    fs.appendFileSync("./output.txt", line.toString() + "\n");
+});
 
 
-  });
+
+
+
+
+// var rl = new LineReader(appConfig.inputfile);
+//   rl.on('line',function(lineno,line) {
+//     currentLine=line;
+//         var prefix=currentLine.split(":");
+//         if(prefix[0]=='Cust'){
+//         queryService.queryProcessing(prefix[1],appConfig.vfsAccessToken,function(err,responseFromApi){
+//             console.log('RESPONSE FROM API :'+responseFromApi);
+//             });
+//           expectedResponse=new Array();
+//           }else if (prefix[0]=='Bot') {
+//             expectedResponse.push(prefix[1]);
+//           }
+// if(expectedResponse.length>0){
+// var result=checkResponse(responseFromApi,expectedResponse);
+//   if(result){
+//     tcPassCount++;
+//   }
+//   else{
+//     failedLines.push(lineno);
+//     tcFailCount++;
+//   }
+// }
+
+
+//   });
 
   rl.on('end', function () {
-
-
-        logService.logResponse(tcPassCount,tcFailCount,failedLines);
-
+    console.log("DATA to log the result");
+  console.log("RESULT IS::"+tcPassCount+"FAIL::"+tcFailCount+"FAILED LINES"+failedLines);
+  logService.logResponse(tcPassCount,tcFailCount,failedLines);
+  console.log("DATA LOGGED");
   });
 rl.on('error',function(err){
     console.log(err);
 });
-
+    
 
 
 }
@@ -79,13 +78,4 @@ else{
 console.log("test case failed");
 return false;
 }
-}
-
-function pushToMap(lineNumber, respString) {
-    var respArray = responseMap.get(lineNumber);
-    if(!respArray) {
-        respArray = new Array();
-        responseMap.set(lineNumber, respArray);
-    }
-    respArray.push(respString);
 }
