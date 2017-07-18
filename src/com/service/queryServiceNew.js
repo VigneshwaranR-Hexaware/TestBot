4a14d8d17764470759084271a32f70b891572daf
Shruti Viswanath <37351@hexaware.com>
2017-07-18 14:39:08 +0530

    tst

--- src/com/service/queryServiceNew.js
+++ src/com/service/queryServiceNew.js
@@ -55,8 +55,8 @@ console.log("LINE NO AFTER SPLIT::"+questAndLine);
       //console.log(key + " : " + value);
     });
     var linetempno=questAndLine[0];
-    console.log("line temp no::"+linetempno);
-    expectedResponse= responseMap.get(linetempno);
+    console.log("line temp no::"+linetempno+typeOf(linetempno)));
+    expectedResponse= responseMap.get(1);
 console.log("EXPECTED RESPONSE::"+expectedResponse);
     var result=checkResponse(message,expectedResponse);
 
