f562a913b152327051a382d462f5bb10f50614c0
Shruti Viswanath <37351@hexaware.com>
2017-07-19 12:23:15 +0530

    to test micro

--- src/com/config/appConfig.js
+++ src/com/config/appConfig.js
@@ -3,3 +3,4 @@ module.exports.vfsAccessToken='Bearer 2236694c3ac943ce93d21afe990b841d';
 module.exports.expectedOutputFile='./data/sampleExpectedResponse.txt';
 module.exports.inputfile='./src/data/VFSComplaintRequest.txt';
 module.exports.tcInputfile='./src/data/inputFile.txt';
+module.exports.cityMicroAccessToken='Bearer 7842094f16b048ed9e53d81e0264bc87';
