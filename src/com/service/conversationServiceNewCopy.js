c5cf9b085836c7f55f801a4bac7e545398835cf4
Shruti Viswanath <37351@hexaware.com>
2017-07-19 12:22:40 +0530

    to test micro

---
+++ src/com/service/conversationServiceNewCopy.js
@@ -0,0 +1,117 @@
+//var inputfile = require('../data/VFSComplaintRequest.txt');
+var LineReader = require('linereader');
+
+var jsUtil=require('util');
+
+var appConfig = require('../config/appConfig.js');
+var logService=require('./logService');
+var QueryService=require('./queryServiceNew');
+var util=require('../config/util.js');
+
+const responseMap = new Map();
+
+
+processRequest();
+
+
+
+function processRequest() {
+const fs = require('fs');
+var currentLine=null;
+var expectedResponse=[];
+var failedLines=[];
+var responseFromApi=null;
+var tcPassCount=0;
+var tcFailCount=0;
+
+var rl = new LineReader(appConfig.inputfile);
+var custLineNo = -1;
+var readQuestiong = -1;
+var quest = new Array();
+  rl.on('line',function(lineno,line) {
+    currentLine=line;
+
+        var prefix=currentLine.split(":");
+
+
+
+        if(prefix[0]=='Cust'){
+                //var queryServ = new queryProcessing(prefix[1]);
+                custLineNo = lineno;
+                //logMsg("LINE NO"+custLineNo);
+                quest.push(lineno+'::'+prefix[1]);
+
+                /*if(readQuestiong == 2) {
+                  //logMsg("Giving call "+quest);
+                  responseMap.forEach(function(value, key) {
+                      logMsg(key + " : " + value);
+                  });
+                    var queryServ = new QueryService.QueryProcessor(responseMap, lineno, quest);
+                }*/
+                readQuestiong++;
+                //var queryServ = new QueryService.QueryProcessor(prefix[1], lineno);
+                //logMsg(queryServ.processCompleted);
+                /*
+
+                }*/
+
+                //setTimeout(logMsg("Waiting"), 1000);
+
+                //queryService.queryProcessing(prefix[1], lineno, responseMap);
+                expectedResponse=new Array();
+          }else if (prefix[0]=='Bot') {
+              //logMsg("LINE NO in bot"+custLineNo);
+                pushToMap(custLineNo, prefix[1]);
+          }
+/*if(expectedResponse.length>0){
+var result=checkResponse(responseFromApi,expectedResponse);
+  if(result){
+    tcPassCount++;
+  }
+  else{
+    failedLines.push(lineno);
+    tcFailCount++;
+  }
+}*/
+
+
+  });
+
+  rl.on('end', function () {
+
+var queryServ = new QueryService.QueryProcessor(responseMap, quest);
+      //  logService.logResponse(tcPassCount,tcFailCount,failedLines);
+
+  });
+rl.on('error',function(err){
+    logMsg(err);
+});
+
+
+
+}
+
+function checkResponse(responseFromApi,expectedResponse ){
+  logMsg("API::"+responseFromApi+"EXPECTED::"+expectedResponse);
+if(expectedResponse.indexOf(responseFromApi) > -1) {
+  logMsg("test case passed");
+  return true;
+}
+else{
+logMsg("test case failed");
+return false;
+}
+}
+
+function pushToMap(lineNumber, respString) {
+    var respArray = responseMap.get(lineNumber);
+    if(!respArray) {
+        respArray = new Array();
+        responseMap.set(lineNumber, respArray);
+    }
+    respArray.push(respString);
+}
+
+var logMsg = function(str) {
+    //console.log(str);
+}
---
+++ src/com/service/queryServiceNewCopy.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const https= require('https');
+var http = require('http');
+var apiai = require('apiai');
+const bodyParser = require('body-parser');
+var request = require("request");
+
+const JSONbig = require('json-bigint');
+const assert = require('assert');
+const appConfig= require('../config/appConfig.js');
+const logger= require('./logService.js');
+var util=require('../config/util.js');
+
+var expectedResponse=[];
+var tcPassCount=0;
+var tcFailCount=0;
+
+//Function Call
+//function preparingResponse(){
+// var response=queryProcessing('Hi',appConfig.developerAccessToken);
+//
+//}
+
+//Processing Query Parameter
+function QueryProcessor(responseMap,questArray) {
+
+  var processCompleted = false;
+
+  if(questArray.length > 0 ) {
+    var questAndLine = questArray.shift().split("::");
+
+
+
+    var options = {
+    method: 'POST',
+    url: 'https://api.api.ai/v1/query',
+    qs: { v: '20150910' },
+    headers:
+     {
+       'cache-control': 'no-cache',
+       'content-type': 'application/json',
+       authorization: appConfig.cityMicroAccessToken
+     },
+    body: {
+        query: [questAndLine[1]], lang: 'en', sessionId: '1234567'
+    },
+    json: true
+  };
+
+  var handleResp = function(error,response, body){
+         var message= util.getMsgFromResp(error, response, body);
+          //logMsg(message+" lin nu is::"+questAndLine[0]);
+
+          logMsg("RESP MAP SIZE IN in query servixce::"+responseMap.size);
+          responseMap.forEach(function(value, key) {
+          //logMsg(key + " : " + value);
+          });
+          var linetempno=questAndLine[0];
+          //logMsg("line temp no::"+linetempno);
+          expectedResponse= responseMap.get(parseInt(linetempno));
+        //logMsg("type is "+(parseInt(linetempno)));
+    //logMsg("EXPECTED RESPONSE::"+expectedResponse);
+        var result=checkResponse(message,expectedResponse);
+        var status = "failed";
+        if(result) {
+            status = "Passed";
+        }
+
+          logger.logConvResult(linetempno, questAndLine[1], expectedResponse, message, status);
+
+          QueryProcessor(responseMap, questArray);
+  }
+
+
+      request(options,handleResp);
+
+  }
+
+
+}
+
+
+function checkResponse(responseFromApi,expectedResponse ){
+      logMsg("API::"+responseFromApi+"EXPECTED::"+expectedResponse);
+
+    if(expectedResponse && expectedResponse.indexOf(responseFromApi) > -1) {
+      logMsg("test case passed");
+      return true;
+    }
+    else{
+    logMsg("test case failed");
+    return false;
+}
+}
+
+var logMsg = function(str) {
+    //console.log(str);
+}
+
+
+module.exports.QueryProcessor=QueryProcessor;
