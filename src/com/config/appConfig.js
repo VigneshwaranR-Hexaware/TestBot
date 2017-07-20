161580f760a89e8ec259e703a94a80eec726daf2
ShrutiVi-hexaware <ShrutiVi.hexaware@gmail.com>
2017-07-20 13:15:10 +0530

    Update appConfig.js

--- src/com/config/appConfig.js
+++ src/com/config/appConfig.js
@@ -1,7 +1,7 @@
-module.exports.developerAccessToken='Bearer 7842094f16b048ed9e53d81e0264bc87';
+module.exports.developerAccessToken='Bearer 2236694c3ac943ce93d21afe990b841d';
 module.exports.vfsAccessToken='Bearer 2236694c3ac943ce93d21afe990b841d';
 module.exports.expectedOutputFile='./data/sampleExpectedResponse.txt';
-module.exports.inputfile='./src/data/CityMicrostrategyTest.txt';
+module.exports.inputfile='./src/data/VFSComplaintRequest.txt';
 module.exports.tcInputfile='./src/data/inputFile.txt';
 module.exports.cityMicroAccessToken='Bearer 7842094f16b048ed9e53d81e0264bc87';
 module.exports.citiinputfile='./src/data/CityMicrostrategyTest.txt';
