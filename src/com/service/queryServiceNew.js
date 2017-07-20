5db6d36a2b78eb6d2dc549589e20c9956b97d73d
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-07-20 11:33:52 +0530

    Updated

--- src/com/service/queryServiceNew.js
+++ src/com/service/queryServiceNew.js
@@ -42,21 +42,18 @@ function QueryProcessor(responseMap,questArray) {
 
           var linetempno=questAndLine[0];
           expectedResponse= responseMap.get(parseInt(linetempno));
-          var respObj=expectedResponse.toString();
+          var respObj=convertArrayToString(expectedResponse.toString());
           var status="";
+          var inputMessage=convertArrayToString(message);
           try
           {
-            assert.deepEqual(message,respObj);
+            assert.deepEqual(inputMessage,respObj);
             status = "Passed";
           }
         catch(e){
           logMsg(e.message);
           status = "failed";
         }
-//        var status = "failed";
-//        if(result) {
-//            status = "Passed";
-//        }
           logger.logConvResult(linetempno, questAndLine[1], expectedResponse, message, status);
           QueryProcessor(responseMap, questArray);
   }
@@ -65,7 +62,14 @@ function QueryProcessor(responseMap,questArray) {
 
 }
 
-
+function convertArrayToString(string){
+  var tempArray=[];
+  if(string.indexOf('')>0){
+    var temp=string.split("");
+    tempArray.push(temp);
+    return tempArray;
+  }
+}
 
 
 function checkResponse(responseFromApi,expectedResponse ){
