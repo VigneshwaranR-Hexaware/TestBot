7028bdab311e036d98d6048df4ca55d38fd580cd
Shruti Viswanath <37351@hexaware.com>
2017-07-25 14:44:16 +0530

    log file added

--- src/com/config/appConfig.js
+++ src/com/config/appConfig.js
@@ -6,3 +6,4 @@ module.exports.tcInputfile='./src/data/inputFile.txt';
 module.exports.cityMicroAccessToken='Bearer 7842094f16b048ed9e53d81e0264bc87';
 module.exports.citiinputfile='./src/data/CityMicrostrategyTest.txt';
 module.exports.vfsinputfile='./src/data/VFSComplaintRequest.txt';
+module.exports.logfile='./logfile.txt';
