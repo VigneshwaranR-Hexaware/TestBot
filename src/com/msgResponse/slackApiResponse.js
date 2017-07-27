e4091d59e760ac59bdb1e7fdc5705263cc0a4283
Deena B <23567@Hexaware.com>
2017-07-27 16:42:58 +0530

    Corrections

--- src/com/msgResponse/slackApiResponse.js
+++ src/com/msgResponse/slackApiResponse.js
@@ -1,5 +1,59 @@
 var lookupResp=function(body){
+  var type=[];
+  var obj=(body.result.fulfillment.messages.length);
+  for(i=0;i<=obj;i++){
+   var typeOf = body.result.fulfillment.messages[i].type;
+   var  apiRespObj = new apiResponsePOJO.apiResponseObject(); 
+   switch(typeOf){
+       
+        case 0:// text response
+        
+          if (!error && response.statusCode === 200) {
+             apiRespObj.sppech=JSON.stringify(body.result.fulfillment.speech);
+             return apiRespObj;
+          }
+           break;
+        case 1:
+        // card
+        
+        
+          if (!error && response.statusCode === 200) {
+            apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
+            apiRespObj.subtitle=JSON.stringify(body.result.fulfillment.subtitle);
+            apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
+             return apiRespObj;
+          }
+                 break;
+        case 2:// quickreply
+        
+            if (!error && response.statusCode === 200) {
+                apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
+            return apiRespObj;
+          }
+                 break;
+        case 3://imageUrl
+            
+                if (!error && response.statusCode === 200) {
+                    apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
+                return apiRespObj;
+            }
+                 break;
+        case 4:// custome payload
+            
+                    if (!error && response.statusCode === 200) {
+                        apiRespObj.payload=JSON.stringify(body.result.fulfillment.payload);
+                    return apiRespObj;
+                }
+                break;
+       default:
+
+           break;
+     }
 
 }
 
-module.exports.getslackResp=lookupResp;
+//module.exports.fbResp= lookupResp; 
+
+}
+
+module.exports.slackResp=lookupResp;
