dd609f076ebd59ef34b3e32844c857795131049a
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-07-27 08:42:28 +0530

    Updated Util

--- src/com/config/util.js
+++ src/com/config/util.js
@@ -1,7 +1,7 @@
 
 var getMessage = function getMessageResponse (error, response, body) {
    if (error) throw new Error(error);
-    console.log("Messages"+ JSON.stringify(body.result.fulfillment.messages));
+    console.log("Messages"+ JSON.stringify(body.result.fulfillment.messages.type));
     var message=JSON.stringify(body.result.fulfillment.speech);
     //console.log(JSON.stringify(body.result));
     if (!error && response.statusCode === 200) {
