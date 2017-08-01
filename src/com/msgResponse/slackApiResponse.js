2e2ac3fa71ff235e2590852420d12a3ce77bdf43
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-08-01 16:05:21 +0530

    Updated

--- src/com/msgResponse/slackApiResponse.js
+++ src/com/msgResponse/slackApiResponse.js
@@ -2,7 +2,7 @@ var apiResponsePOJO=require('../config/apiResponsePOJO.js');
 
 var lookupResp=function(error,response,body){
      console.log("INSIDE SLACK FILE");
-  var type=[];
+  var respObjArr=[];
   var obj=(body.result.fulfillment.messages.length);
    console.log("INSIDE SLACK FILE--MESSAGE LENGTH::"+obj);
     //console.log("INSIDE SLACK FILE--MESSAGE::"+JSON.stringify(body.result.fulfillment.messages[0]));
@@ -25,37 +25,46 @@ var lookupResp=function(error,response,body){
                   case 0:// text response
 
                       if (!error && response.statusCode === 200) {
+                          var  apiRespObj = new apiResponsePOJO.apiResponseObject();
                           apiRespObj.speech=JSON.stringify(body.result.fulfillment.speech);
                           console.log("SPEECH:::"+apiRespObj.speech);
+                          respObjArr.push(apiRespObj);
                           //return apiRespObj;
                       }
                   break;
                   case 1:// card
                       if ( !error &&response.statusCode === 200) {
+                          var  apiRespObj = new apiResponsePOJO.apiResponseObject();
                           apiRespObj.title = platform_msg.title;
                           apiRespObj.subtitle = platform_msg.subtitle;
                           apiRespObj.imageUrl = platform_msg.imageUrl;
+                          respObjArr.push(apiRespObj);
                           console.log("TITLE:::"+apiRespObj.title);
                         //  return apiRespObj;
                       }
                   break;
                   case 2:// quickreply
                       if (!error && response.statusCode === 200) {
+                          var  apiRespObj = new apiResponsePOJO.apiResponseObject();
                           apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
                           console.log("TITLEwwww:::"+apiRespObj.title);
+                          respObjArr.push(apiRespObj);
                       //return apiRespObj;
                       }
                   break;
                   case 3://imageUrl
                       if (!error &&response.statusCode === 200) {
+                          var  apiRespObj = new apiResponsePOJO.apiResponseObject();
                           apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
+                          respObjArr.push(apiRespObj);
                     //  return apiRespObj;
                        }
                   break;
                   case 4:// custome payload
                           if (!error && response.statusCode === 200) {
                               apiRespObj.payload=JSON.stringify(body.result.fulfillment.payload);
-                        //  return apiRespObj;
+                              respObjArr.push(apiRespObj);
+                              //  return apiRespObj;
                           }
                   break;
                   default:
