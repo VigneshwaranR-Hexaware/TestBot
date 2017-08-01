41ae4098bec844d7329740426f13111bfcd965a5
Shruti Viswanath <37351@hexaware.com>
2017-08-01 13:12:47 +0530

    corrections on slack

--- src/com/msgResponse/slackApiResponse.js
+++ src/com/msgResponse/slackApiResponse.js
@@ -13,58 +13,57 @@ var lookupResp=function(error,response,body){
     var platform_msg = body.result.fulfillment.messages[i];
     if(platform_msg) {
           var platform_compare=platform_msg.platform;
-    }
+          console.log("PLATFORM:::"+platform_compare);
+          if(platform_compare){
+              if (platform_compare=="slack")
+              {
+                  var typeOf = body.result.fulfillment.messages[i].type;
+                  console.log("INSIDE SLACK FILE--TYPE::"+typeOf);
+                  var  apiRespObj = new apiResponsePOJO.apiResponseObject();
+                  switch(typeOf){
 
-    console.log("PLATFORM:::"+platform_compare);
-    if(platform_compare){
-        if (platform_compare=="slack")
-        {
-            var typeOf = body.result.fulfillment.messages[i].type;
-            console.log("INSIDE SLACK FILE--TYPE::"+typeOf);
-            var  apiRespObj = new apiResponsePOJO.apiResponseObject();
-            switch(typeOf){
+                  case 0:// text response
 
-            case 0:// text response
+                      if (!error && response.statusCode === 200) {
+                          apiRespObj.speech=JSON.stringify(body.result.fulfillment.speech);
+                          console.log("SPEECH:::"+apiRespObj.speech);
+                          //return apiRespObj;
+                      }
+                  break;
+                  case 1:// card
+                      if ( !error &&response.statusCode === 200) {
+                          apiRespObj.title = platform_msg.title;
+                          apiRespObj.subtitle = platform_msg.subtitle;
+                          apiRespObj.imageUrl = platform_msg.imageUrl;
+                          console.log("TITLE:::"+apiRespObj.title);
+                        //  return apiRespObj;
+                      }
+                  break;
+                  case 2:// quickreply
+                      if (!error && response.statusCode === 200) {
+                          apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
+                          console.log("TITLEwwww:::"+apiRespObj.title);
+                      //return apiRespObj;
+                      }
+                  break;
+                  case 3://imageUrl
+                      if (!error &&response.statusCode === 200) {
+                          apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
+                    //  return apiRespObj;
+                       }
+                  break;
+                  case 4:// custome payload
+                          if (!error && response.statusCode === 200) {
+                              apiRespObj.payload=JSON.stringify(body.result.fulfillment.payload);
+                        //  return apiRespObj;
+                          }
+                  break;
+                  default:
+                  break;
+              }
 
-                if (!error && response.statusCode === 200) {
-                    apiRespObj.speech=JSON.stringify(body.result.fulfillment.speech);
-                    console.log("SPEECH:::"+apiRespObj.speech);
-                    //return apiRespObj;
-                }
-            break;
-            case 1:// card
-                if ( !error &&response.statusCode === 200) {
-                    apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
-                    apiRespObj.subtitle=JSON.stringify(body.result.fulfillment.subtitle);
-                    apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
-                    console.log("TITLE:::"+apiRespObj.title);
-                  //  return apiRespObj;
-                }
-            break;
-            case 2:// quickreply
-                if (!error && response.statusCode === 200) {
-                    apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
-                    console.log("TITLEwwww:::"+apiRespObj.title);
-                //return apiRespObj;
-                }
-            break;
-            case 3://imageUrl
-                if (!error &&response.statusCode === 200) {
-                    apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
-              //  return apiRespObj;
-                 }
-            break;
-            case 4:// custome payload
-                    if (!error && response.statusCode === 200) {
-                        apiRespObj.payload=JSON.stringify(body.result.fulfillment.payload);
-                  //  return apiRespObj;
-                    }
-            break;
-            default:
-            break;
-        }
-
-    }
+          }
+      }
 }
 else{
 
