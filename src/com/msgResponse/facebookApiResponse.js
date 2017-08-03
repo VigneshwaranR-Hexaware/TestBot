42c5456e3440a0f37ab9538eda1054a40551ce0c
Shruti Viswanath <37351@hexaware.com>
2017-08-03 15:24:44 +0530

    Corrections

--- src/com/msgResponse/facebookApiResponse.js
+++ src/com/msgResponse/facebookApiResponse.js
@@ -1,4 +1,5 @@
 var apiResponsePOJO=require('../config/apiResponsePOJO.js');
+var responseType = require('../util/respType.js');
 
 var lookupResp=function(error,response,body){
 
@@ -19,9 +20,10 @@ var lookupResp=function(error,response,body){
                   case 0:// text response
                        if(!error && response.statusCode === 200) {
                        var apiRespObj = new apiResponsePOJO.apiResponseObject();
-                       apiRespObj.speech=JSON.stringify(body.result.fulfillment.speech).replace(/"/g, "");
-                       console.log("SPEECH:::"+apiRespObj.speech);
+                       apiRespObj.speech=JSON.stringify(platform_msg.speech).replace(/"/g, "");
+                       console.log("SPEECH FB:::"+apiRespObj.speech);
                        //  return apiRespObj;
+                       apiRespObj.respType=responseType.SPEECH;
                        responceObject.push(apiRespObj);
                      }
                      break;
@@ -29,11 +31,12 @@ var lookupResp=function(error,response,body){
                    case 1:// card
                         if(!error && response.statusCode === 200) {
                         var apiRespObj = new apiResponsePOJO.apiResponseObject();
-                        apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
-                        apiRespObj.subtitle=JSON.stringify(body.result.fulfillment.subtitle);
-                        apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
-                        console.log("card:::"+apiRespObj.title);
+                        apiRespObj.title=platform_msg.title;
+                        apiRespObj.subtitle=platform_msg.subtitle;
+                        apiRespObj.imageUrl=platform_msg.imageUrl;
+                        console.log("card FB:::"+apiRespObj.title);
                         //return apiRespObj;
+                        apiRespObj.respType=responseType.CAROUSEL;
                         responceObject.push(apiRespObj);
                      }
                      break;
@@ -41,9 +44,10 @@ var lookupResp=function(error,response,body){
                    case 2:// queck repl
                         if(!error && response.statusCode === 200) {
                         var apiRespObj = new apiResponsePOJO.apiResponseObject();
-                        apiRespObj.title=JSON.stringify(body.result.fulfillment.title);
-                        console.log("quest repl:::"+apiRespObj.title);
+                        apiRespObj.title=platform_msg.title;
+                        console.log("quest repl: FB::"+apiRespObj.title);
                         //return apiRespObj;
+                        apiRespObj.respType=responseType.QUICKREPLY;
                         responceObject.push(apiRespObj);
                       }
                       break;
@@ -51,8 +55,9 @@ var lookupResp=function(error,response,body){
                   case 3://imageUrl
                        if(!error && response.statusCode === 200) {
                        var apiRespObj = new apiResponsePOJO.apiResponseObject();
-                       apiRespObj.imageUrl=JSON.stringify(body.result.fulfillment.imageUrl);
+                       apiRespObj.imageUrl=platform_msg.imageUrl;
                        console.log("imageUrl:::"+apiRespObj.imageUrl);
+                       apiRespObj.respType=responseType.IMAGE;
                         //return apiRespObj;
                       responceObject.push(apiRespObj);
                       }
@@ -61,9 +66,10 @@ var lookupResp=function(error,response,body){
                   case 4:// custome
                         if(!error && response.statusCode === 200) {
                         var apiRespObj = new apiResponsePOJO.apiResponseObject();
-                        apiRespObj.payload=JSON.stringify(body.result.fulfillment.payload);
+                        apiRespObj.payload=platform_msg.payload;
                         console.log("custome:::"+apiRespObj.payload);
                         //return apiRespObj;
+                        apiRespObj.respType=responseType.PAYLOAD;
                         responceObject.push(apiRespObj);
                        }
                        break;
@@ -74,6 +80,17 @@ var lookupResp=function(error,response,body){
                 }
               }
             }
+            console.log("TO PRINT THE RSP OBJ"+JSON.stringify(responceObject));
+            if(!(responceObject.length>0)){
+              console.log("INSIDE DEFAULT");
+              var apiRespObj = new apiResponsePOJO.apiResponseObject();
+              apiRespObj.speech=body.result.fulfillment.messages[0].speech.replace(/"/g, "");
+              console.log("SPEECH FB DFAULT:::"+apiRespObj.speech);
+              //  return apiRespObj;
+              responceObject.push(apiRespObj);
+            }
+
+
             return responceObject;
-           };
-module.exports.fbResp= lookupResp;
+           }
+module.exports.lookupResp= lookupResp;
--- src/com/util/respType.js
+++ src/com/util/respType.js
@@ -1,4 +1,6 @@
 
 module.exports.SPEECH = 0;
 module.exports.CAROUSEL = 1;
+module.exports.QUICKREPLY=3;
 module.exports.IMAGE=2;
+module.exports.PAYLOAD=4;
