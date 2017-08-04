b72b652ceefe55b62dbca6cbfe1aeeb92715b35c
Shruti Viswanath <37351@hexaware.com>
2017-08-04 11:43:59 +0530

    Corrections

--- src/com/config/appConfig.js
+++ src/com/config/appConfig.js
@@ -1,7 +1,8 @@
-module.exports.developerAccessToken='Bearer 2236694c3ac943ce93d21afe990b841d';
+module.exports.developerAccessToken='Bearer 4b81cbd1686c416bad563b7ea2f4a36d';
 module.exports.vfsAccessToken='Bearer 2236694c3ac943ce93d21afe990b841d';
 module.exports.expectedOutputFile='./data/sampleExpectedResponse.txt';
-module.exports.inputfile='./src/data/VFSComplaintRequest.txt';
+module.exports.inputfile='./src/data/VFSComplaintRequestFB.txt';
 module.exports.tcInputfile='./src/data/inputFile.txt';
 module.exports.cityMicroAccessToken='Bearer 7842094f16b048ed9e53d81e0264bc87';
 module.exports.citiinputfile='./src/data/CityMicrostrategyTest.txt';
+module.exports.vfsfacebookaccess="Bearer 4b81cbd1686c416bad563b7ea2f4a36d";
