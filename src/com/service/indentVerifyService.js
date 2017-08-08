5db3a9ba429ed4c973166c146d01bf6a82b92cd4
Jeyam pandi Bose (jp) <32128@hexaware.com>
2017-08-08 15:38:07 +0530

    indent verify service

---
+++ src/com/service/indentVerifyService.js
@@ -0,0 +1,81 @@
+var LineReader = require('linereader');
+var jsUtil=require('util');
+var appConfig = require('../config/appConfig.js');
+var logService=require('./logService');
+var QueryService=require('./queryServiceNew');
+var util=require('../config/util.js');
+var appConst = require('../util/appConstants.js');
+var expectedRespObj = require('../config/apiResponsePOJO.js');
+
+function processRequest() {
+    const fs = require('fs');
+
+    var utterances = new Array();;
+
+
+    var reader = new LineReader(appConfig.YWSinputfile);
+
+    var quest = new Array();
+      reader.on('line',function(lineno,line) {
+            utterances.push(line);
+      });
+
+      reader.on('end', function () {
+            var failedUtterances = new Array();;
+            var tcPassedCount = 0;
+            var tcFailedCount = 0;
+            var totalTC = 0;
+            var queryServ = new QueryService.QueryProcessor(responseMap, quest);
+      });
+
+      reader.on('error',function(err){
+            logMsg(err);
+      });
+}
+
+var logMsg = function(str) {
+    logger.traceData(str);
+}
+
+function checkUtterances(utterances, failedUtterances, tcPassedCount, tcFailedCount, totalTC) {
+    var processCompleted = false;
+    if(utterances.length > 0 ) {
+          totalTC ++;
+          var utteranceToTest = questArray.shift();
+          var options = {
+              method: 'POST',
+              url: 'https://api.api.ai/v1/query',
+              qs: { v: '20150910' },
+              headers:
+              {
+                 'cache-control': 'no-cache',
+                 'content-type': 'application/json',
+                  authorization: appConfig.developerAccessToken
+              },
+              body: {
+                  query: [utteranceToTest], lang: 'en', sessionId: '1234567'
+              },
+              json: true
+          };
+
+     var handleResp = function(error, response, body){
+
+          var failed = false;
+
+          //Logic to check the intent should come here
+
+
+          if(failed || error) {
+              failedUtterances.push(utteranceToTest);
+              tcFailedCount++;
+          } else {
+              tcPassCount ++;
+          }
+
+          checkUtterances(utterances, failedUtterances, tcPassedCount, tcFailedCount, totalTC);
+     }
+
+      request(options,handleResp);
+  }
+
+}
