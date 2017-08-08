df8a5f4db8afe6bcc500871acabcae0ce002ddac
Shruti Viswanath <37351@hexaware.com>
2017-08-08 12:26:35 +0530

    Corrections

--- src/com/config/appConfig.js
+++ src/com/config/appConfig.js
@@ -1,4 +1,4 @@
-module.exports.developerAccessToken='Bearer 7f8b1e54bc5a4a1a93e0fe776d0ce0bb';
+module.exports.developerAccessToken='Bearer c857028100e04eb2b0e9cbe15c779146';
 module.exports.vfsAccessToken='Bearer 2236694c3ac943ce93d21afe990b841d';
 module.exports.expectedOutputFile='./data/sampleExpectedResponse.txt';
 module.exports.inputfile='./src/data/VFSComplaintRequestFB.txt';
@@ -9,3 +9,5 @@ module.exports.vfsfacebookaccess="Bearer 4b81cbd1686c416bad563b7ea2f4a36d";
 module.exports.platform='facebook';
 module.exports.biinputfile='./src/data/BiServiceAgentTest.txt';
 module.exports.biagentAccessToken='Bearer 7f8b1e54bc5a4a1a93e0fe776d0ce0bb';
+module.exports.YWSinputfile='./src/data/YWSTestCase.txt';
+module.exports.YWSaccesstoken='Bearer c857028100e04eb2b0e9cbe15c779146';
