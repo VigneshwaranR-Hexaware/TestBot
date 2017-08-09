63278e0ba53831fd226590b6f21f59ee54e62073
Shruti Viswanath <37351@hexaware.com>
2017-08-09 13:06:33 +0530

    Corrections

--- src/com/service/queryServiceNew.js
+++ src/com/service/queryServiceNew.js
@@ -95,11 +95,14 @@ function checkResponse(testUnitInd, lineNo, custSays, botResponse, expectedResp)
                         break;
 
                         case responseType.PAYLOAD:
-                            if(processingBotResp.title)
+                            if(processingBotResp.title){
                             var testTitleResult = checkStringResponse(processingBotResp.title, processingExpResp.title);
-                            if(processingBotResp.subtitle)
+                            console.log("title result"+testTitleResult);
+                          }
+                            if(processingBotResp.subtitle){
                             var testSubtitleResult = checkStringResponse(processingBotResp.subtitle, processingExpResp.subtitle);
-
+                            console.log("subtitle result"+testSubtitleResult);
+                          }
                             if(processingBotResp.custPayloadTitle){
                             var testpayloadtitleResult = checkArrayResponse(processingBotResp.custPayloadTitle, processingExpResp.custPayloadTitle);
                           //  console.log("testpayloadtitle result::"+testpayloadtitleResult);
