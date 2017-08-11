b38c36018f04951d1a990192c2408c96c94b6442
Rakesh Jha <29140@corp.hexaware.com>
2017-08-11 11:18:05 +0530

    update

--- src/com/msgResponse/apiAiResponse.js
+++ src/com/msgResponse/apiAiResponse.js
@@ -6,13 +6,14 @@ var getApiResp=function(error,response,body){
 var apiRespArray=[];
 var obj=(body.result.fulfillment.messages.length);
     for(i=0;i<=obj;i++){
+      console.log("INSIDE SLACK FILE--TYPEdsadsa::"+body.result.fulfillment.messages[i]);
         var typeOf = body.result.fulfillment.messages[i].type;
-        logMsg("INSIDE SLACK FILE--TYPE::"+typeOf);
+        console.log("INSIDE SLACK FILE--TYPE::"+typeOf);
             if(typeOf == 0){
                 if (!error && response.statusCode === 200) {
                     var  apiRespObj = new apiResponsePOJO.apiResponseObject();
                     apiRespObj.speech=JSON.stringify(body.result.fulfillment.messages[i].speech);
-                    logMsg("SPEECH:::"+apiRespObj.speech);
+                    console.log("SPEECH:::"+apiRespObj.speech);
                     apiRespObj.respType=responseType.SPEECH;
                     apiRespArray.push(apiRespObj);
                 }
