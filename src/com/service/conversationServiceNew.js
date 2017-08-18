808d0c729a037478d1ae07df2d6b63cf03729331
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-08-18 19:27:42 +0530

    Updated Index.js

--- src/com/service/conversationServiceNew.js
+++ src/com/service/conversationServiceNew.js
@@ -107,5 +107,5 @@ var handleCustPayload = function(expectedResp, expRespObj) {
         expRespObj.custPayloadTitle = expectedResp;
     }
 }
-processRequest();
+Module.exports = processRequest();
 //console.log(parseExpectedResp("IMAGE--Hi--Good Morning--Good Evening--Good Night--Good Afternoon"));
