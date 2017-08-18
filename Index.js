504a15c0c32381bdc4bf21ffe4dddfda8938f4fc
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-08-18 15:03:04 +0530

    Updated Index.js

--- Index.js
+++ Index.js
@@ -3,32 +3,12 @@ const https= require('https');
 var apiai = require('apiai');
 var request = require("request");
 const assert = require('assert');
-
 var arrayequals = require('array-equal');
+var queryService=require('./src/com/service/conversationServiceNew.js');
 
-var arry1=['Watch on Youtube,What to do?,What causes this?,Another query,Thats all,Contact Us'];
-var arry2=['Watch on Youtube,What to do?,What causes this?,Another query,Thats all,Contact Us'];
-var testpayloadtitleResult = checkPayloadResponse(arry1, arry2);
-console.log("result::"+testpayloadtitleResult);
 
-function checkPayloadResponse(responseFromApi,expectedResponse ){
 
-/*for(i=0;i<responseFromApi.length;i++){
-  var result;
-  console.log(responseFromApi[i]+responseFromApi.length);
-  if(responseFromApi[i].indexOf(expectedResponse) > -1) {
-    console.log("inside");
-      result=true;
-  }else{
-    result=false;
-    break;
-      }
+queryService.processRequest();
 
-}*/
-      if(responseFromApi && expectedResponse) {
-        return(arrayequals(responseFromApi, expectedResponse)) ;
-      }
-      return result;
 
-}
-//queryService.preparingResponse();
+
